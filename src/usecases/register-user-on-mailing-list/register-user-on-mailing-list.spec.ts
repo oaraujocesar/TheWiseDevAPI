d869e0c6fed661f7021c168a887daf6cc8a1cf7d
César O. Araújo <oaraujocesar@gmail.com>
2022-03-13 21:33:59 -0300

    test: should add user with complete data to mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,19 @@
+import { User } from './user'
+
+describe('Register user on mailing list use case', () => {
+  it('should add user with complete data to mailing list', async () => {
+    const users: User[] = []
+
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
+    //   repo
+    // )
+    // const name = 'fake_name'
+    // const email = 'fake@email.com'
+    // const response = await usecase.RegisterUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('fake@email.com')
+
+    // expect((await user).name).toBe('fake_name')
+  })
+})
