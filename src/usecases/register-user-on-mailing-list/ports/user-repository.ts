1a7af3fb8b15196b36ddd49437712aedaa01597f
César O. Araújo <oaraujocesar@gmail.com>
2022-03-13 22:32:11 -0300

    feat: allow null as response from findUserByEmail

--- src/usecases/register-user-on-mailing-list/ports/user-repository.ts
+++ src/usecases/register-user-on-mailing-list/ports/user-repository.ts
@@ -2,7 +2,7 @@ import { User } from '../user'
 
 export interface UserRepository {
   add(user: User): Promise<void>
-  findUserByEmail(email: string): Promise<User>
+  findUserByEmail(email: string): Promise<User | null>
   findAllUsers(): Promise<User[]>
   isUserRegistered(user: User): Promise<boolean>
 }
